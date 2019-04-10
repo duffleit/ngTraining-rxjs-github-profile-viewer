9833e25a01eb0f4ad66a9da7cbc73fc24935fe36
David Leitner <david@leitner.io>
2019-04-10 02:22:05 +0200

    Add github fetching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GithubProfileViewerComponent } from './github-profile-viewer/github-profile-viewer.component';
@@ -10,6 +11,7 @@ import { GithubProfileViewerComponent } from './github-profile-viewer/github-pro
     GithubProfileViewerComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
--- src/app/github-profile-viewer/github-profile-viewer.component.ts
+++ src/app/github-profile-viewer/github-profile-viewer.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-github-profile-viewer',
   template: `
     <input (keyup)="changeUsername($event.target.value)" [value]="userName" />
+    <div>
+      {{ githubProfile$ | async | json }}
+    </div>
   `,
   styleUrls: ['./github-profile-viewer.component.css']
 })
 export class GithubProfileViewerComponent {
   public userName = '';
-
-  constructor() {}
+  public githubProfile$;
+  constructor(private http: HttpClient) {}
 
   public changeUsername(value: string): void {
     this.userName = value;
+    this.githubProfile$ = this.http.get(
+      `https://api.github.com/users/${this.userName}`
+    );
   }
 }
