6e1933ae3d5931ec9c8c3ebac67543f9c0fd9d14
David Leitner <david@leitner.io>
2019-04-10 02:46:26 +0200

    Add Error Handling

--- src/app/github-profile-viewer/github-profile-viewer.component.ts
+++ src/app/github-profile-viewer/github-profile-viewer.component.ts
@@ -1,30 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { debounceTime, mergeMap } from 'rxjs/operators';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { debounceTime, mergeMap, tap, catchError } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-github-profile-viewer',
   template: `
     <input (keyup)="changeUsername($event.target.value)" [value]="userName" />
-    <div>
-      {{ githubProfile$ | async | json }}
+    <div *ngIf="(githubProfile$ | async); else loading; let userName">
+      <h2>{{ userName.login }}</h2>
+      <img [src]="userName.avatar_url" />
+      <p>Location: {{ userName.location }}</p>
+      <p>Bio: {{ userName.bio }}</p>
     </div>
+    <ng-template #loading>
+      <div *ngIf="errorObject">Error</div>
+      <div *ngIf="!errorObject">
+        Loading...
+      </div>
+    </ng-template>
   `,
   styleUrls: ['./github-profile-viewer.component.css']
 })
 export class GithubProfileViewerComponent implements OnInit {
   public userName$ = new BehaviorSubject<string>('');
   public githubProfile$: Observable<any>;
-  
+  public errorObject = null;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.userName$.pipe(debounceTime(1000)).subscribe(userName => {
-      this.githubProfile$ = this.http.get(
-        `https://api.github.com/users/${userName}`
-      );
-    });
+    this.userName$
+      .pipe(
+        tap(() => {
+          this.errorObject = null;
+        }),
+        debounceTime(1000)
+      )
+      .subscribe(userName => {
+        this.githubProfile$ = this.http
+          .get(`https://api.github.com/users/${userName}`)
+          .pipe(
+            catchError(error => {
+              this.errorObject = error;
+              return throwError(error);
+            })
+          );
+      });
   }
 
   get userName(): string {
